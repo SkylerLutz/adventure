3659f1c2bff4e4c69f99924156d16b2b6e48e226
SkylerLutz <spl5@njit.edu>
2013-09-19 17:25:26 -0400

    Added aliases for enumeration constants, to more easily recognize them when the user enters text

--- Action.java
+++ Action.java
@@ -1,34 +1,41 @@
 public enum Action {
 
-	ActionLook(0),	
-	ActionGoEast(1),
-	ActionGoWest(-1),
-	ActionGoSouth(-2),
-	ActionGoNorth(2),
+	ActionLook(0, new String[]{"look", "l"}),	
+	ActionGoEast(1, new String[]{"east", "e"}),
+	ActionGoWest(-1, new String[]{"west", "w"}),
+	ActionGoSouth(-2, new String[]{"south", "s"}),
+	ActionGoNorth(2, new String[]{"north", "n"}),
 
-	ActionGoNortheast(3),
-	ActionGoNorthwest(-3),
-	ActionGoSoutheast(4),
-	ActionGoSouthwest(-4),
+	ActionGoNortheast(3, new String[]{"northeast", "ne"}),
+	ActionGoNorthwest(-3, new String[]{"northwest", "nw"}),
+	ActionGoSoutheast(4, new String[]{"southeast", "se"}),
+	ActionGoSouthwest(-4, new String[]{"southwest", "sw"}),
 
 
-	ActionPickUp(5),
-	ActionDig(6),
-	ActionBreak(7),
-	ActionInspect(8),
+	ActionPickUp(5, new String[]{"pickup", "pick up", "get"}),
+	ActionDig(6, new String[]{"dig"}),
+	ActionBreak(7, new String[]{"break", "destroy"}),
+	ActionInspect(8, new String[]{"inspect", "examine"}),
 	ActionThrow(9),
 	ActionJump(10),
 	ActionShake(11),
+	ActionViewItems(12, new String[]{"inventory", "items"}),
 
 	ActionUnknown(666);
 
 	Action(int code) {
+		this(code, new String[]{});
+	}
+	Action(int code, String[] aliases) {
 		this.code = code;
+		this.aliases = aliases;
 	}
 	int getCode() {
 		return this.code;
 	}
-
+	String[] getAliases() {
+		return this.aliases;
+	}
 
 	
 	static Action getOppositeDirection(Action a) {
@@ -43,4 +50,5 @@ public enum Action {
 		return result;
 	}
 	protected int code;
+	protected String[] aliases;
 };
