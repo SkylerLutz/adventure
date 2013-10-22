0008762047e9e16042b310ec1642e33e18e31302
SkylerLutz <spl5@njit.edu>
2013-10-22 13:01:22 -0400

    Freefall currently runs on a background thread

--- RoomSky.java
+++ RoomSky.java
@@ -6,23 +6,30 @@ public class RoomSky extends Room {
 	}
 	public void freefall() {
 
-		for(int i=0; i < this.duration; i++) {
+		final int dur = this.duration;
+		final Player p = this.player;
+		Thread thread = new Thread(new Runnable() {
+			public void run() {
+				for(int i=0; i < dur; i++) {
 
-			try {
-				Thread.sleep(400);
-				System.out.println((this.duration-i)*1000 + " thousand feet.");
+					try {
+						Thread.sleep(400);
+						System.out.println((dur-i)*1000 + " thousand feet.");
 
-				for(int j=0; j < 3; j++) {
-					System.out.println("...");
-					Thread.sleep(200);
+						for(int j=0; j < 3; j++) {
+							System.out.println("...");
+							Thread.sleep(200);
+						}
+					}
+					catch(Exception e1) {
+						
+					}
 				}
+				System.out.println("You hit the ground");
+				p.die();
 			}
-			catch(Exception e1) {
-				
-			}
-		}
-		System.out.println("You hit the ground");
-		this.player.die();
+		});
+		thread.start();
 	}
 	public void breakFall() {
 		// transition to field
