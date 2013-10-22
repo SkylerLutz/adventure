34104dbc6079056a9ba91f36fe3992f268832989
SkylerLutz <spl5@njit.edu>
2013-10-22 11:04:26 -0400

    RoomSky--Room for when the user is parachuting through the sky

---
+++ RoomSky.java
@@ -0,0 +1,23 @@
+public class RoomSky extends Room {
+
+	public RoomSky(String description, String shortDescription, int duration) {
+		super(description, shortDescription);
+	}
+	public void freefall() {
+
+		for(int i=0; i < this.duration; i++) {
+
+			Thread.sleep(400);
+			System.out.println((this.duration-1)*1000 + " thousand feet.");
+
+			for(int j=0; j < 3; j++) {
+				System.out.println("...");
+				Thread.sleep(200);
+			}
+		}
+	}
+	public void breakfall() {
+		// transition to field
+	}
+	protected int duration; // in seconds
+}
